3df0c810b9502623743695d15cee837f0b80b927
Khaleel Musleh <khaleelmusleh@Khaleels-MacBook-Pro.local>
2022-02-11 16:11:22 -0800

    almost done

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -21,23 +21,27 @@ router.get('/:id', async (req, res) => {
     }) 
 
     router.post('/', async (req, res) => {
-        let { name, description } = req.body
+        let { description, notes } = req.body
+
         const newAction = await Actions.insert(req.body)
-        if(!name && !name.trim() || !description && !description.trim()) {
+
+        if(!description && !description.trim() || !notes && !notes.trim()) {
             res.status(400).json({ message: `Project  not found` })
         } else {
              res.json(newAction)
         }   
+        // res.json(newAction)
+
     })
 
     router.put('/:id', async (req, res) => {
-        let { name, description } = req.body
+        let { description, notes } = req.body
 
 
         const updatedAction = await Actions.update(req.params.id, req.body)
         if(!req.params.id) {
             res.status(404).json({ message: `Project with ID cannot insert` })
-        }  if (!name || !description) {
+        }  if (!description || !notes) {
             res.status(400).json({ message: `Project  missing name or content` })
         } else {
              res.json(updatedAction)
